3b00b2ae0c550d66057b9881b80b54ab7df86d79
Michael Neely <michael@michaelneely.net>
2020-03-12 23:17:23 -0400

    Added regex to web.php

--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/{any?}', function () {
     return view('welcome');
-});
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+})->where('any', '^(?!api\/)[\/\w\.-]*');
 
 Auth::routes();
 
