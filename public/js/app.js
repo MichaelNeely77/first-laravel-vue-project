ad0bcaf470b08e875a7bc6c2b835d6bdf3ed8e3a
Michael Neely <michael@michaelneely.net>
2020-03-18 23:11:30 -0400

    got columns straightened out with computed properties

--- public/js/app.js
+++ public/js/app.js
@@ -1958,6 +1958,13 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
+//
+//
+//
+//
+//
+//
 
 /* harmony default export */ __webpack_exports__["default"] = ({
   components: {
@@ -1975,14 +1982,22 @@ __webpack_require__.r(__webpack_exports__);
       return this.bookables == null ? 0 : Math.ceil(this.bookables.length / this.columns);
     }
   },
+  methods: {
+    bookablesInRow: function bookablesInRow(row) {
+      return this.bookables.slice((row - 1) * this.columns, row * this.columns);
+    },
+    placeholdersInRow: function placeholdersInRow(row) {
+      return this.columns - this.bookablesInRow(row).length;
+    }
+  },
   created: function created() {
     var _this = this;
 
     this.loading = true;
     setTimeout(function () {
       _this.bookables = [{
-        title: "Cheap Villa!!!",
-        content: "A very cheap villa you will love"
+        title: "Awesome Cheap Villa",
+        content: "A very cheap villa that you and your family will love"
       }, {
         title: "Awesome Cheap Villa 2",
         content: "A very cheap villa that you and your family will love"
@@ -37501,17 +37516,39 @@ var render = function() {
       ? _c("div", [_vm._v("\n        Data is loading...\n    ")])
       : _c(
           "div",
-          _vm._l(_vm.bookables, function(bookable, index) {
-            return _c("bookable-list-item", {
-              key: index,
-              attrs: {
-                "item-title": bookable.title,
-                "item-content": bookable.content,
-                price: 1000
-              }
-            })
+          _vm._l(_vm.rows, function(row) {
+            return _c(
+              "div",
+              { key: "row" + row, staticClass: "row" },
+              [
+                _vm._l(_vm.bookablesInRow(row), function(bookable, column) {
+                  return _c(
+                    "div",
+                    { key: "row" + row + column, staticClass: "col" },
+                    [
+                      _c("bookable-list-item", {
+                        attrs: {
+                          "item-title": bookable.title,
+                          "item-content": bookable.content,
+                          price: 1000
+                        }
+                      })
+                    ],
+                    1
+                  )
+                }),
+                _vm._v(" "),
+                _vm._l(_vm.placeholdersInRow(row), function(p) {
+                  return _c("div", {
+                    key: "placeholder" + row + p,
+                    staticClass: "col"
+                  })
+                })
+              ],
+              2
+            )
           }),
-          1
+          0
         )
   ])
 }
--- resources/js/bookables/Bookables.vue
+++ resources/js/bookables/Bookables.vue
@@ -5,14 +5,21 @@
             Data is loading...
         </div>
         <div v-else>
-            <bookable-list-item 
-            :item-title="bookable.title" 
-            :item-content="bookable.content" 
-            :price="1000"
-            v-for="(bookable, index) in bookables"
-            :key="index"
-            >
-            </bookable-list-item>
+            <div class="row" v-for="row in rows" :key="'row' + row">
+                <div 
+                    class="col" 
+                    v-for="(bookable, column) in bookablesInRow(row)" 
+                    :key="'row' + row + column">
+                    <bookable-list-item 
+                        :item-title="bookable.title" 
+                        :item-content="bookable.content" 
+                        :price="1000"
+                    >
+                    </bookable-list-item>
+                </div>
+                <div class="col" v-for="p in placeholdersInRow(row)" :key="'placeholder' + row + p"></div>
+            </div>
+
         </div>
     </div>
 </template>
@@ -37,12 +44,19 @@ export default {
                 : Math.ceil(this.bookables.length / this.columns);
             }    
         },
+        methods: {
+            bookablesInRow(row) {
+                return this.bookables.slice((row - 1) * this.columns, row * this.columns);
+            },
+            placeholdersInRow(row) {      
+                return this.columns - this.bookablesInRow(row).length;
+ }        },
     created() {
         this.loading = true;
         setTimeout(() =>{
             this.bookables = [{
-                title: "Cheap Villa!!!",
-                content: "A very cheap villa you will love"
+                title: "Awesome Cheap Villa",
+                content: "A very cheap villa that you and your family will love"
             },
             {
                 title: "Awesome Cheap Villa 2",
