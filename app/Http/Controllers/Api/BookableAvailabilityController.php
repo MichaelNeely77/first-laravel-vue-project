b9ede0672b45853692fd6021e50879d9f620ff78
Michael Neely <michael@michaelneely.net>
2020-03-27 23:53:14 -0400

    Added more query builders to the BookableAvailabilityController

--- app/Http/Controllers/Api/BookableAvailabilityController.php
+++ app/Http/Controllers/Api/BookableAvailabilityController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Bookable;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -13,14 +14,16 @@ class BookableAvailabilityController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function __invoke(Request $request)
+    public function __invoke($id,Request $request)
     {
         $data = $request->validate([
             'from' => 'required|date_format:Y-m-d|after:now',
             'to' => 'required|date_format:Y-m-d|after_or_equal:from'
 
         ]);
-        dd($data);
+        $bookable = Bookable::findOrFail($id);
+
+        dd($bookable->bookings());
     }
         
 }
