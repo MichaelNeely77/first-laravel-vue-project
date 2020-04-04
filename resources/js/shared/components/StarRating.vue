f0f8ea301da12ca51f1dcdf8b03cf993ceaf4879
Michael Neely <michael@michaelneely.net>
2020-04-03 23:56:50 -0400

    Added the star functionality

--- resources/js/shared/components/StarRating.vue
+++ resources/js/shared/components/StarRating.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="d-flex">
+        Actual rating is: {{ rating }};
         <i class="fas fa-star" v-for="star in fullStars" :key="'full' + star"></i>
         <i class="fas fa-star-half-alt" v-if="halfStar"></i>
         <i class="far fa-star" v-for="star in emptyStars" :key="'empty' + star"></i>
@@ -14,14 +15,29 @@ export default {
     },
     computed: {
         halfStar() {
-            return true;
+            const fraction = Math.round((this.rating - Math.floor(this.rating)) * 100);
+
+            console.log(fraction);
+
+            return fraction > 0 && fraction < 50;
         },
         fullStars() {
-            return 3;
+
+            return Math.round(this.rating);
         },
         emptyStars() {
-            return 1;
+            return 5 - Math.ceil(this.rating);
         }
     },
+    created() {
+        const numbers = [0.9, 4.0, 4.4, 4.5, 4.6, 4.9];
+
+        // numbers.forEach(n => {
+        //     console.log(`round for ${n} is ${Math.round(n)}`);
+        //     console.log(`floor for ${n} is ${Math.floor(n)}`);
+        //     console.log(`ceil for ${n} is ${Math.ceil(n)}`);
+        //     console.log('================================');
+        // });
+    }
 }
 </script>
