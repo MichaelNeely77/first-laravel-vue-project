38a121f796c9a852d06de433dcbbcffec179c2cc
Michael Neely <michael@michaelneely.net>
2020-04-04 23:44:18 -0400

    Added Rfeview page

---
+++ resources/js/review/Review.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <div class="form-group">
+            <label for="" class="text-muted">Select the star rating (1 is worst - 5 is best)</label>
+            <star-rating :rating="5" class="fa-3x"></star-rating>
+        </div>
+        <div class="form-group">
+            <label for="content" class="text-muted">Describe your experience with us</label>
+        </div>
+        <textarea name="content" id="" cols="30" rows="10" class="form-control"></textarea>
+        <button class="btn btn-primary btn-lg btn-block mt-2">Submit</button>
+    </div>
+</template>
--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,6 +1,7 @@
 import VueRouter from "vue-router";
 import Bookables from "./bookables/Bookables";
-import Bookable from "./bookable/Bookable"
+import Bookable from "./bookable/Bookable";
+import Review from "./review/Review";
 
 const routes = [
     {
@@ -12,7 +13,14 @@ const routes = [
         path: "/bookable/:id",
         component: Bookable,
         name: "bookable",
+    },
+    {
+        path: "/review/:id",
+        component: Review,
+        name: "review"
     }
+    
+
 
 ];
 
