31c0234fddefafcb70bb546e9684d2a247b6b657
Michael Neely <michael@michaelneely.net>
2020-04-09 23:59:21 -0400

    Added further functionality to the ReviewComtroller

--- app/Http/Controllers/Api/ReviewController.php
+++ app/Http/Controllers/Api/ReviewController.php
@@ -12,4 +12,29 @@ class ReviewController extends Controller
     public function show($id) {
         return new ReviewResource(Review::findOrFail($id));
     }
+
+    public function store(Request $request) {
+        $data = $request->validate([
+            'id' => 'required|size:36',
+            'content' => 'required|min:2',
+            'rating' => 'required|in:1,2,3,4,5'
+        ]);
+
+        $booking = Booking::findByReviewKey($data['id']);
+
+        if (null === $booking) {
+            return abort(404);
+        }
+
+        $booking->review_key = '';
+        $booking->save();
+
+        $review = Review::make($data);
+        $review->booking_id = $booking_id;
+        $review->bookable_id->bookable_id;
+        $review->save();
+
+        return $review;
+
+    }
 }
