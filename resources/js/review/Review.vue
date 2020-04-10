5bf7ee8cfc7b5563222a978bd3800dc961223d96
Michael Neely <michael@michaelneely.net>
2020-04-09 23:46:49 -0400

    Added the code for the Stayed at section of Review.vue

--- resources/js/review/Review.vue
+++ resources/js/review/Review.vue
@@ -1,6 +1,23 @@
 <template>
-    <div>
-        <div v-if="loading">Loading...</div>
+    <div class="row">
+        <div :class="[{'col-md-4': loading || !alreadyReviewed}, {'d-none': !loading && alreadyReviewed}]">
+            <div class="card-body">
+                <div v-if="loading">Loading...</div>
+                <div v-else>
+                    <p>
+                        Stayed at <router-link :to="{name: 'bookable', params: {id: booking.bookable.bookable_id}}">
+                            {{ booking.bookable.title}}
+                        </router-link>
+                    </p>
+                    <p>
+                       From {{ booking.from }} to {{ booking.to }}
+                    </p>
+                    
+                </div>
+            </div>
+        </div>
+        <div :class="[{'col-md-8': loading || !alreadyReviewed}, {'col-md-12': !loading && alreadyReviewed}]">
+                    <div v-if="loading">Loading...</div>
         <div v-else>
                     <div v-if="alreadyReviewed">
             <h3>You've already left a review for this booking</h3>
@@ -21,6 +38,9 @@
         </div>
 
         </div>
+
+        </div>
+
         
     </div>
 </template>
@@ -58,8 +78,13 @@ export default {
     },
     computed: {
         alreadyReviewed() {
+            return this.hasReview || !this.booking;
+        },
+        hasReview() {
             return this.existingReview != null;
-        }
+        },
+        hasBooking() {
+            return this.booking != null;        }
     }
 };
 </script>
