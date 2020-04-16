6ae0432db7b54ef3cf810f88e21b4bf692f5c505
Michael Neely <michael@michaelneely.net>
2020-04-16 00:03:52 -0400

    Set store and mutations for date selection

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,7 +10,8 @@ import moment from 'moment';
 import StarRating from './shared/components/StarRating';
 import FatalError from './shared/components/FatalError';
 import Success from './shared/components/Success';
-import ValidationErrors from './shared/components/ValidationErrors';
+import ValidationErrors from './shared/components/ValidationErrors'
+import storeDefinition from './store';
 
 window.Vue = require('vue');
 Vue.use(VueRouter);
@@ -23,9 +24,12 @@ Vue.component("fatal-error", FatalError);
 Vue.component("success", Success);
 Vue.component("v-errors", ValidationErrors);
 
+const store = new Vuex.Store(storeDefinition);
+
 const app = new Vue({
     el: '#app',
     router,
+    store,
     components: {
         index: Index,
     }
