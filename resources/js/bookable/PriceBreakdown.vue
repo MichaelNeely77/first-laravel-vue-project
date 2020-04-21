46df355837813eb65de9850158300ea1dc4b0964
Michael Neely <michael@michaelneely.net>
2020-04-20 23:57:50 -0400

    Adding the PriceBreakdown component

---
+++ resources/js/bookable/PriceBreakdown.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h6 class="text-uppercase text-secondary font-weight-bolder">Price Breakdown</h6>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
